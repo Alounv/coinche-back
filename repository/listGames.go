5a1f0267b63caeb6d0545dd5b99dd432d3f7f009
alounv <alounv@gmail.com>
2022-02-19 15:54:00 +0100

    Clean ListGames

--- repository/listGames.go
+++ repository/listGames.go
@@ -2,7 +2,42 @@ package repository
 
 import (
 	"coinche/domain"
+	"database/sql"
+
+	"github.com/jmoiron/sqlx"
 )
+
+func getGame(rows *sql.Rows, db *sqlx.DB) (domain.Game, error) {
+	var game domain.Game
+	err := rows.Scan(
+		&game.ID,
+		&game.Name,
+		&game.CreatedAt,
+		&game.Phase,
+	)
+	if err != nil {
+		return domain.Game{}, err
+	}
+
+	rows, err = db.Query("SELECT name, team FROM player WHERE gameid=$1", game.ID)
+	if err != nil {
+		return domain.Game{}, err
+	}
+
+	game.Players = map[string]domain.Player{}
+
+	for rows.Next() {
+		var playerName string
+		var teamName string
+		err = rows.Scan(&playerName, &teamName)
+		game.Players[playerName] = domain.Player{Team: teamName}
+		if err != nil {
+			return domain.Game{}, err
+		}
+	}
+
+	return game, nil
+}
 
 func (s *GameRepository) ListGames() ([]domain.Game, error) {
 	var games []domain.Game
@@ -13,34 +48,10 @@ func (s *GameRepository) ListGames() ([]domain.Game, error) {
 	}
 
 	for rows.Next() {
-		var game domain.Game
-		err = rows.Scan(
-			&game.ID,
-			&game.Name,
-			&game.CreatedAt,
-			&game.Phase,
-		)
+		game, err := getGame(rows, s.db)
 		if err != nil {
 			return nil, err
 		}
-
-		rows, err := s.db.Query("SELECT name, team FROM player WHERE gameid=$1", game.ID)
-		if err != nil {
-			return nil, err
-		}
-
-		game.Players = map[string]domain.Player{}
-
-		for rows.Next() {
-			var playerName string
-			var teamName string
-			err = rows.Scan(&playerName, &teamName)
-			game.Players[playerName] = domain.Player{Team: teamName}
-			if err != nil {
-				return nil, err
-			}
-		}
-
 		games = append(games, game)
 	}
 
